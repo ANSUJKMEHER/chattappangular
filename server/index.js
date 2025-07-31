2fdf0a7df3964682400f704222ecd4b8ff103379
ANSUJKMEHER <ansujkmeher@gmail.com>
2025-07-31 12:10:54 +0530

    Updated package.json and added server files

--- server/index.js
+++ server/index.js
@@ -10,30 +10,27 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*" }
 });
+
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  socket.on("join", ({ userId, room }) => {
+  socket.on("join", (userId) => {
     socket.data.userId = userId;
-    socket.data.room = room;
-    socket.join(room);
-    io.to(room).emit("message", { user: "System", text: `${userId} joined room ${room}` });
+    io.emit("message", { user: "System", text: `${userId} joined the chat` });
   });
 
   socket.on("sendMessage", (data) => {
-    io.to(data.room).emit("message", { user: data.user, text: data.text });
+    io.emit("message", { user: data.user, text: data.text });
   });
 
   socket.on("disconnect", () => {
-    const room = socket.data.room;
-    const userId = socket.data.userId;
-    if (room && userId) {
-      io.to(room).emit("message", { user: "System", text: `${userId} left the room` });
-    }
+    if (socket.data.userId)
+      io.emit("message", { user: "System", text: `${socket.data.userId} left the chat` });
     console.log("User disconnected:", socket.id);
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+server.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on http://0.0.0.0:3000");
 });
+
